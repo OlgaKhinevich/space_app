923ae4f1d1a1c29849b117be08e71ebb1260b3c5
Olga Khinevich <olga-khinevich@yandex.ru>
2021-08-27 15:44:40 +0300

    init

--- store/results.js
+++ store/results.js
@@ -5,13 +5,5 @@ export const state = () => ({
 export const mutations = ({
     setResults(state, newData) {
         state.results = newData;
-    },
-    /*changeIsFavourite(state, itemId) {
-        state.resulte = state.results.map((el) => {
-            if (el.data[0].nasa_id === itemId) { 
-                el.isFavourite = !el.isFavourite;  
-            }
-        }) 
-       
-    }*/
+    }
 })
